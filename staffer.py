245de9920c35e3afb68475aa013438a91bdf1942
anselm <abairdsmith@gmail.com>
2025-01-20 18:47:42 +0100

    Only one command "staff" takes either a directory or a file and does the right thing.

--- staffer.py
+++ staffer.py
@@ -326,9 +326,21 @@ def list_directory(datadir: Path) -> List[Path]:
     return files
 
 
+def do_staff(file: Path, refresh: bool, do_plot: bool, dont_show: bool):
+    pages = pdf2numpy(file, refresh=refresh)
+    for page_number, page in enumerate(pages):
+        try:
+            staff = find_staff(page, do_plot=do_plot)
+            image = draw_staff(staff, background=page.copy())
+            if (not dont_show) and show(image):
+                return True
+        except Exception as e:
+            print(f"{file}, page: {page_number}, no staff found:\n\t{e}")
+
+
 @click.command()
 @click.argument("path",
-                type=click.Path(exists=True, file_okay=False, dir_okay=True),
+                type=click.Path(file_okay=True, dir_okay=True),
                 default="/home/anselm/Downloads/KernSheet/")
 @click.option("--plot", "do_plot", is_flag=True, default=False,
               help="Debug - Plots various data as we go.")
@@ -338,32 +350,16 @@ def list_directory(datadir: Path) -> List[Path]:
               help="Don't use cached .numpy files, refresh them.")
 @click.option("--dont-show", "dont_show", is_flag=True, default=False,
               help="Don't show image + staff after processing.")
-def all(path: Path, do_plot: bool, refresh: bool, dont_show: bool):
-    dataset = list_directory(Path(path))
-    for path in dataset:
-        print(path)
-        pages = pdf2numpy(path, refresh=refresh)
-        for page in pages:
-            staff = find_staff(page, do_plot=do_plot)
-            image = draw_staff(staff, background=page.copy())
-            if (not dont_show) and show(image):
-                return
-
-
-@click.command()
-@click.argument("path", type=click.Path(exists=False))
-@click.option("--plot", "do_plot", is_flag=True, default=False,
-              help="Debug - Plots various data as we go.")
-@click.option("--refresh", "refresh", is_flag=True, default=False,
-              help="Don't use cached .numpy files, refresh them.")
-def staff(path: Path, do_plot: bool, refresh: bool):
+def staff(path: Path, do_plot: bool, refresh: bool, dont_show: bool):
     path = Path(path)
-    pages = pdf2numpy(path, refresh=refresh)
-    for page in pages:
-        staff = find_staff(page, do_plot=do_plot)
-        image = draw_staff(staff, background=page.copy())
-        if show(image):
-            return
+    if path.is_dir():
+        dataset = list_directory(Path(path))
+        for path in dataset:
+            print(path)
+            if do_staff(path, refresh, do_plot, dont_show):
+                break
+    else:
+        do_staff(path, refresh, do_plot, dont_show)
 
 
 @click.group
@@ -371,7 +367,6 @@ def cli():
     pass
 
 
-cli.add_command(all)
 cli.add_command(staff)
 
 if __name__ == "__main__":
