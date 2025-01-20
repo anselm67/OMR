fc91eb07153ae27de3a30906fcca30e22add202f
anselm <abairdsmith@gmail.com>
2025-01-20 12:49:06 +0100

    This is getting pretty good, at least from a visual point of view. Staffs amd bars are detected in over 20 pdfs.

--- pdf2img.py
+++ pdf2img.py
@@ -22,12 +22,21 @@ SIZE = (1800, 1200)
 
 @dataclass
 class Staff:
-    top_offset: int
-    left: int
-    right: int
+    rh_top: int
+    lh_bot: int
+    bars: List[int]
+
+
+@dataclass
+class Page:
     # For each double staff (right hand, left hand)
     # (rh_top: top of rh, lh_bot: bottom of lh)
-    positions: List[Tuple[int, int]]
+    staves: List[Staff]
+
+
+def is_binary(image: MatLike) -> bool:
+    height, width = image.shape
+    return np.sum(image == 0) + np.sum(image == 255) == height * width
 
 
 def save_numpy(path: Path, array: List[MatLike]):
@@ -40,7 +49,7 @@ def load_numpy(path: Path) -> List[MatLike]:
         return cast(List[MatLike], pickle.load(f))
 
 
-def denoise(image: MatLike, block_size: int = 11, C: int = 2) -> MatLike:
+def denoise(image: MatLike, block_size: int = -1, C: int = 2) -> MatLike:
     if block_size > 0:
         return cv2.adaptiveThreshold(
             image,
@@ -75,16 +84,20 @@ def deskew(
     min_rotation_angle_degrees=0.05,
     hough_thresold: int = 240
 ):
-    avg_angle = average_angle(image, hough_thresold=hough_thresold)
-    angle = np.degrees(avg_angle) - 90.0
+    def get_angle(image: MatLike) -> float:
+        avg_angle = average_angle(image, hough_thresold=hough_thresold)
+        return np.degrees(avg_angle) - 90.0
+
+    angle = get_angle(image)
     if angle < min_rotation_angle_degrees:
         # For a 1200px width, 600px center to side:
         # height = 600 * sin(0.1 degrees) ~ 1 px
         return image
-    print(f"\tapplying {angle:2.4f} rotation.")
     height, width = image.shape
     matrix = cv2.getRotationMatrix2D((width // 2, height//2), angle, 1)
-    return cv2.warpAffine(image, matrix, (width, height))
+    image = cv2.warpAffine(image, matrix, (width, height))
+    print(f"\t{angle:2.4f} rotation => {get_angle(image):2.4f} degrees.")
+    return image
 
 
 def pdf2numpy(
@@ -98,16 +111,19 @@ def pdf2numpy(
     # Runs the pdf conversion with all transformations.
     pdf = convert_from_path(path.with_suffix(".pdf"))
 
-    def transform(image: MatLike) -> MatLike:
-        image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2GRAY)
+    def transform(orig_image: MatLike) -> MatLike:
+        image = cv2.cvtColor(np.array(orig_image), cv2.COLOR_RGB2GRAY)
         image = denoise(image)
         image = deskew(image)
         # Rescale to requested width and pad to requested height.
         h, w = image.shape
         scale = width / w
-        return cv2.resize(
+        image = cv2.resize(
             image, (width, int(h * scale)), interpolation=cv2.INTER_AREA
         )
+        # Some of the operations above de-binarize the images.
+        image = denoise(image)
+        return image
 
     # Caches the results.
     images = [transform(np.array(image)) for _, image in enumerate(pdf)]
@@ -158,10 +174,9 @@ def compare(a: List[MatLike], b: List[MatLike], crop=None):
 
 
 def draw_staff(
-    staff: Staff,
+    page: Page,
     shape: Optional[Tuple[int, int]] = None,
     background: Optional[MatLike] = None,
-    padding: int = 50,
     thickness: int = 2
 ) -> MatLike:
     if shape is None:
@@ -173,45 +188,30 @@ def draw_staff(
         image = np.full(shape, 255, dtype=np.uint8)
     rgb_image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
     staff_color = (255, 0, 0)
-    for rh_top, lh_bot in staff.positions:
-        # Right hand staff:
+    for staff in page.staves:
+        # Draws the bars.
+        for x in staff.bars:
+            cv2.line(
+                rgb_image,
+                (x, staff.rh_top),
+                (x, staff.lh_bot),
+                staff_color, thickness
+            )
+        # Draws the top most and bottom most staff lines:
         cv2.line(
             rgb_image,
-            (staff.left - padding, rh_top),
-            (staff.right + padding, rh_top),
+            (staff.bars[0], staff.rh_top),
+            (staff.bars[-1], staff.rh_top),
             staff_color, thickness
         )
         # Left hand staff
-        cv2.line(  # type: ignore
+        cv2.line(
             rgb_image,
-            (staff.left - padding, lh_bot),
-            (staff.right + padding, lh_bot),
+            (staff.bars[0], staff.lh_bot),
+            (staff.bars[-1], staff.lh_bot),
             staff_color, thickness
         )
-        bottom = lh_bot
-    cv2.line(
-        rgb_image,
-        (staff.left, staff.top_offset),
-        (staff.left, bottom),
-        staff_color, thickness
-    )
-    cv2.line(
-        rgb_image,
-        (staff.right, staff.top_offset),
-        (staff.right, bottom),
-        staff_color, thickness
-    )
     return rgb_image
-
-
-def dedup(y_lines: MatLike, min_gap: int = 2) -> MatLike:
-    if not y_lines.size or len(y_lines) <= 1:
-        return y_lines
-    output = [y_lines[0]]
-    for y in y_lines[1:]:
-        if y - output[-1] > min_gap:
-            output.append(y)
-    return np.array(output)
 
 
 def line_indices(lines: List[int]) -> List[Tuple[int, int]]:
@@ -223,14 +223,35 @@ def line_indices(lines: List[int]) -> List[Tuple[int, int]]:
     return out
 
 
-def find_staff(image: MatLike, do_plot: bool = True) -> Staff:
+def find_bars(
+        image: MatLike, fill_rate: float = 0.85, do_plot: bool = True
+) -> List[int]:
+    x_proj = np.sum(image, 0)
+    # A bar goes from top to bottom, so we can compute a min height for
+    # our peak by assuming a percentage fill rate on that vertical line.
+    peak_height = int(image.shape[0] * fill_rate * 255)
+    print(f"peak_height: {peak_height}")
+    bar_peaks, properties = find_peaks(
+        x_proj, distance=50,  height=peak_height)
+    bar_peaks = [p.item() for p in bar_peaks]
+    # bar_heights = [h.item() for h in properties['peak_heights']]
+    # print(list(zip(bar_peaks, bar_heights)))
+
+    if do_plot:
+        plt.subplot()
+        plt.vlines(bar_peaks, ymin=0, ymax=50000, colors='r')
+        plt.plot(x_proj)
+        plt.show()
+
+    return bar_peaks
+
+
+def find_staff(orig_image: MatLike, do_plot: bool = True) -> Page:
     # Computes the vertical and horizontal projections.
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 2))
-    staff_lines = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
-    staff_lines = image
-    staff_lines = cv2.bitwise_not(staff_lines)
-    y_lines = np.sum(staff_lines, 1)
-    x_lines = np.sum(staff_lines, 0)
+    image = cv2.bitwise_not(orig_image)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 1))
+    image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+    y_lines = np.sum(image, 1)
 
     # Computes the moving average of the y projection and gets the peaks
     # of this series to obtain the rough position of each staff.
@@ -239,15 +260,16 @@ def find_staff(image: MatLike, do_plot: bool = True) -> Staff:
     offset = (wsize - 1) // 2
     y_avg = np.convolve(y_lines, np.ones(wsize) / wsize, mode='valid')
     y_proj[offset:offset+len(y_avg)] = y_avg
-    # The width=30 is the max allowed for chopin/mazurka/mazurka07-3
-    staff_peaks, _ = find_peaks(y_proj, width=30, distance=50)
+    # The width=20 is the max allowed for chopin/mazurka/mazurka30-3
+    staff_peaks, _ = find_peaks(y_proj, width=20, distance=50, height=50000)
 
     # Around each staff peak, find the peaks corresponding to each staff line.
     staff_lines = []
     for peak in staff_peaks:
+        peak_height = 75000
         staff_range = y_lines[peak-40:peak+40]
         line_peaks, properties = find_peaks(
-            staff_range, height=75000, distance=7)
+            staff_range, height=peak_height, distance=7)
         if len(line_peaks) > 5:
             # We're pretty lax on our find_peaks() param, in case we
             # get too many peaks, we throw the smallest ones out.
@@ -258,7 +280,12 @@ def find_staff(image: MatLike, do_plot: bool = True) -> Staff:
             )
             line_peaks = [line_peak for line_peak, _ in sorted_peaks]
             line_peaks = line_peaks[:5]
-
+        elif len(line_peaks) < 5:
+            # Lower the thresold until we get 5 lines.
+            while peak_height > 0 and len(line_peaks) < 5:
+                peak_height -= 5000
+                line_peaks, properties = find_peaks(
+                    staff_range, height=peak_height, distance=7)
         staff_lines.extend([p+peak-40 for p in line_peaks])
 
     staff_lines = sorted(staff_lines)
@@ -266,57 +293,33 @@ def find_staff(image: MatLike, do_plot: bool = True) -> Staff:
     if do_plot:
         plt.subplot()
         plt.plot(y_lines, 'b', label='y_lines')
+        plt.plot(y_proj, 'b', label='y_lines', linestyle="dotted")
         plt.vlines(staff_peaks, ymin=0, ymax=300000, colors='r')
         plt.vlines(staff_lines, ymin=0, ymax=100000, colors='g')
         plt.show()
-
-    staff_range = Staff(
-        top_offset=0,
-        left=0, right=0,
-        positions=[],
-    )
-    staff_range = replace(staff_range, top_offset=staff_lines[0])
 
     if len(staff_lines) % 10 != 0:
         raise ValueError(
             f"Number of lines {len(staff_lines)} should be divisible by 10."
         )
-    else:
-        staff_range = replace(staff_range, positions=[
-            (staff_lines[ridx], staff_lines[lidx]) for ridx, lidx in line_indices(staff_lines)])
+
+    page = Page(staves=[])
+    positions = [
+        (staff_lines[ridx], staff_lines[lidx]) for ridx, lidx in line_indices(staff_lines)
+    ]
 
     # Left and right are top and last offsets of vertical lines.
-    x = np.nonzero(x_lines)[0]
-    if x.size == 0:
-        raise ValueError(f"margin-detection: we got a blank image?")
-    staff_range = replace(staff_range, left=x[0], right=x[-1])
+    image = cv2.bitwise_not(orig_image)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 5))
+    image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+    for rh_top, lh_bot in positions:
+        bars = find_bars(image[rh_top:lh_bot, :], do_plot=do_plot)
+        page.staves.append(Staff(rh_top, lh_bot, bars))
 
-    return staff_range
-
-
-def histo(a: MatLike) -> bool:
-    values, counts = np.unique(a, return_counts=True)
-    for v, c in zip(values, counts):
-        print(f"{v}: {c}")
-    return len(values) == 2 and values[0] == 0 and values[1] == 255
+    return page
 
 
-def cut_sheet(image: MatLike, staff: Staff) -> List[MatLike]:
-    interstaff = 0
-    for (rh_top, _), (_, lh_bot) in zip(staff.positions[1:], staff.positions[:-1]):
-        interstaff += (rh_top - lh_bot)
-    interstaff //= len(staff.positions) - 1
-    rolls = []
-    for rh_top, lh_bot in staff.positions:
-        top = rh_top - interstaff // 2
-        bot = lh_bot + interstaff // 2
-        rolls.append(image[top:bot, staff.left:staff.right])
-        if show(rolls[-1]):
-            break
-    return rolls
-
-
-def list(datadir: Path) -> List[Path]:
+def list_directory(datadir: Path) -> List[Path]:
     files = []
     for root, _, filenames in os.walk(datadir):
         for filename in filenames:
@@ -327,28 +330,41 @@ def list(datadir: Path) -> List[Path]:
 
 
 @click.command()
-@click.option("--plot", "do_plot", is_flag=True, default=False)
-def all(do_plot: bool):
-    dataset = list(Path("/home/anselm/Downloads/KernSheet/chopin/mazurka"))
+@click.option("--plot", "do_plot", is_flag=True, default=False,
+              help="Debug - Plots various data as we go.")
+@click.option("--refresh", "refresh", is_flag=True, default=False,
+              help="Don't use cached .numpy files, refresh them.")
+@click.option("--refresh", "refresh", is_flag=True, default=False,
+              help="Don't use cached .numpy files, refresh them.")
+@click.option("--dont-show", "dont_show", is_flag=True, default=False,
+              help="Don't show image + staff after processing.")
+def all(do_plot: bool, refresh: bool, dont_show: bool):
+    dataset = list_directory(
+        Path("/home/anselm/Downloads/KernSheet/chopin/mazurka"))
     for path in dataset:
         print(path)
-        pages = pdf2numpy(path)
+        pages = pdf2numpy(path, refresh=refresh)
         for page in pages:
             staff = find_staff(page, do_plot=do_plot)
             image = draw_staff(staff, background=page)
-            show(image)
+            if (not dont_show) and show(image):
+                return
 
 
 @click.command()
 @click.argument("path", type=click.Path(exists=False))
-@click.option("--plot", "do_plot", is_flag=True, default=False)
-def staff(path: Path, do_plot: bool):
+@click.option("--plot", "do_plot", is_flag=True, default=False,
+              help="Debug - Plots various data as we go.")
+@click.option("--refresh", "refresh", is_flag=True, default=False,
+              help="Don't use cached .numpy files, refresh them.")
+def staff(path: Path, do_plot: bool, refresh: bool):
     path = Path(path)
-    pages = pdf2numpy(path)
+    pages = pdf2numpy(path, refresh=refresh)
     for page in pages:
         staff = find_staff(page, do_plot=do_plot)
         image = draw_staff(staff, background=page)
-        show(image)
+        if show(image):
+            return
 
 
 @click.group
@@ -361,9 +377,3 @@ cli.add_command(staff)
 
 if __name__ == "__main__":
     cli()
-
-# for image in tl:
-#     staff = find_staff(image)
-#     cut_sheet(image, staff)
-#     # if show(create_staff(staff, background=image.copy())):
-#     #     break
