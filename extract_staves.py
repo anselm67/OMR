4660c2509d3bd99e3ba7832e686de070aed5b218
anselm <abairdsmith@gmail.com>
2025-03-03 14:56:48 +0100

    some progress.

--- extract_staves.py
+++ extract_staves.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.nn as nn
+import torchvision
 from cv2.typing import MatLike
 from numpy.typing import NDArray
 
@@ -32,6 +33,7 @@ def to32(i: int) -> int:
 
 
 class Dataset:
+    no_box = -1     # Represents the absence of a box.
     page_divider = 2
     max_width = to32(1200 // page_divider)
     max_height = to32(1825 // page_divider)
@@ -51,81 +53,91 @@ class Dataset:
     def __len__(self) -> int:
         return len(self.data)
 
-    def pick_one(self) -> MatLike:
-        path = random.choice(self.data)
-        tensor, _ = self.load(path)
-        return tensor.numpy()
+    def pick_one(self) -> tuple[torch.Tensor, torch.Tensor]:
+        pages, boxes = self.batch(1, transform=False)
+        return pages[0], boxes[0]
 
-    def load(self, path: Path, transform=False) -> tuple[torch.Tensor, list[BoundingBox]]:
+    def load(self, path: Path, transform=False) -> tuple[torch.Tensor, torch.Tensor]:
         with open(path, "rb") as fp:
             page, crops = cast(
                 tuple[MatLike, list[BoundingBox]],
                 pickle.load(fp)
             )
             height, width, _ = page.shape
-            page = cv2.resize(
-                page, (width // self.page_divider, height // self.page_divider))
-            crops = [(
-                left // self.page_divider,
-                top // self.page_divider,
-                right // self.page_divider,
-                bottom // self.page_divider
-            ) for left, top, right, bottom in crops]
+            target_height, target_width = height // self.page_divider, width // self.page_divider
+            page = cv2.resize(page, (target_width, target_height))
+            crops = torch.stack([torch.Tensor((
+                left / width,
+                top / height,
+                right / width,
+                bottom / height
+            )).to(torch.float32) for left, top, right, bottom in crops])
             if transform:
-                return self.transform(page), crops
+                return cast(tuple[torch.Tensor, torch.Tensor], self.transform(page, crops))
             else:
                 return torch.from_numpy(page), crops
 
     @classmethod
-    def transform(cls, page: MatLike) -> torch.Tensor:
+    def transform(
+        cls, page: MatLike, boxes: Optional[torch.Tensor] = None
+    ) -> tuple[torch.Tensor, Optional[torch.Tensor]]:
+        # Pads the image.
         output = torch.full((cls.max_height, cls.max_width), 255)
         height, width, _ = page.shape
         output[:height, :width] = torch.from_numpy(
             cv2.cvtColor(page, cv2.COLOR_BGR2GRAY)
         )
         output = output.to(torch.float32)
-        return (output - output.mean()) / output.std()
 
-    def batch(self) -> tuple[torch.Tensor, torch.Tensor]:
-        samples = [self.load(path, transform=True)
-                   for path in random.sample(self.data, self.batch_size)]
+        # Scales the boxes accordingly.
+        if boxes is not None:
+            boxes[:, ::2] = width * boxes[:, ::2] / cls.max_width
+            boxes[:, 1::2] = height * boxes[:, 1::2] / cls.max_height
 
-        def bounding_boxes(pages: list[list[BoundingBox]]) -> torch.Tensor:
+        return (output - output.mean()) / output.std(), boxes
+
+    def batch(
+        self, batch_size=batch_size, transform: bool = True
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        samples = [self.load(path, transform=transform)
+                   for path in random.sample(self.data, batch_size)]
+
+        def bounding_boxes(pages: list[torch.Tensor]) -> torch.Tensor:
             empty = torch.full(
-                (self.batch_size, 5 * self.max_staves), 0).to(torch.float32)
-            for batchno, bboxes in enumerate(pages):
-                x = 0
-                for left, top, right, bottom in bboxes:
-                    empty[batchno, x] = left / self.max_width
-                    empty[batchno, x+1] = top / self.max_height
-                    empty[batchno, x+2] = right / self.max_width
-                    empty[batchno, x+3] = bottom / self.max_height
-                    empty[batchno, x+4] = 1.0
-                    x += 5
+                (batch_size, self.max_staves, 5), self.no_box
+            ).to(torch.float32)
+            for batchno, boxes in enumerate(pages):
+                empty[batchno, :len(boxes), 0:4] = boxes
+                empty[batchno, :len(boxes), 4] = 1.0
+                empty[batchno:, len(boxes):, 4] = 0
             return empty
 
         return (
             torch.stack([page for page, _ in samples]),
-            bounding_boxes([page for _, page in samples])
+            bounding_boxes([boxes for _, boxes in samples])
         )
 
     @classmethod
-    def pred2bbox(cls, preds: torch.Tensor, is_logits: bool = True) -> list[PredictedBox]:
+    def pred2bbox(
+        cls, shape: torch.Size, preds: torch.Tensor, is_logits: bool = True
+    ) -> list[PredictedBox]:
         bboxes = list()
         i = 0
-        while i < preds.shape[0]:
-            confidence = torch.sigmoid(preds[i+4]) if is_logits else preds[i+4]
+        height, width = shape[0:2]
+        for i in range(0, preds.shape[0]):
+            confidence = torch.sigmoid(
+                preds[i, 4]) if is_logits else preds[i, 4]
             bboxes.append(((
-                int(preds[i+0] * cls.max_width),
-                int(preds[i+1] * cls.max_height),
-                int(preds[i+2] * cls.max_width),
-                int(preds[i+3] * cls.max_height)),
+                int(preds[i, 0] * width),
+                int(preds[i, 1] * height),
+                int(preds[i, 2] * width),
+                int(preds[i, 3] * height)),
                 confidence
             ))
-            i += 5
         return bboxes
 
     def stats(self):
+        staves = 0
         max_width, max_height, max_staves = 0, 0, 0
         for path in self.data:
             page, crops = self.load(path)
@@ -135,7 +147,11 @@ class Dataset:
                 max(height, max_height),
                 max(len(crops), max_staves)
             )
-        print(f"{max_height=}, {max_width=}, {max_staves=}")
+            staves += len(crops)
+        print(
+            f"{max_height=}, {max_width=}, {max_staves=}, "
+            f"avg_staves={staves / len(self.data):.2f}"
+        )
 
 
 class StafferModel(nn.Module):
@@ -144,11 +160,11 @@ class StafferModel(nn.Module):
         super(StafferModel, self).__init__()
         self.net = nn.Sequential(
             # Layer 1 (BS, 1, H, W) -> (BS, 32, H/16, W/16)
-            nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(1, 16, kernel_size=12, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
             # Layer 2
-            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(16, 32, kernel_size=6, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
             # Layer 3
@@ -159,26 +175,36 @@ class StafferModel(nn.Module):
         self.gap = nn.AdaptiveAvgPool2d(1)
         self.fc = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(64, 8 * 5),
+            nn.Linear(64, Dataset.max_staves * 5),
         )
 
     def forward(self, x: torch.Tensor):
         x = self.net(x)
         x = self.gap(x)
         x = self.fc(x)
-        # Apply sigmoidto the coordinates only.
-        x[:, :4] = torch.sigmoid(x[:, :4])
-        return x
+        # Apply sigmoid to the coordinates only.
+        x = x.view((-1, 8, 5))
+        x[:, :, :4] = torch.sigmoid(x[:, :, :4])
+        return x.view((-1, 8, 5))
 
 
-def draw(page: MatLike, boxes: list[PredictedBox], wait: bool = True) -> bool:
+def draw(page: MatLike,
+         boxes: torch.Tensor,
+         true_boxes: Optional[torch.Tensor] = None,
+         wait: bool = True) -> bool:
     print(f"{len(boxes)} bounding boxes:")
-    for (left, top, right, bottom), confidence in boxes:
+    for (left, top, right, bottom), confidence in Dataset.pred2bbox(page.shape, boxes):
         print(
             f"\t({left}, {top}), ({right}, {bottom}) @ {confidence:.2f}"
         )
-        cv2.rectangle(page, (left, top),
-                      (right, bottom), (255, 0, 0), 2)
+        if confidence > 0.5:
+            cv2.rectangle(page, (left, top),
+                          (right, bottom), (255, 0, 0), 2)
+    if true_boxes is not None:
+        for (left, top, right, bottom), confidence in Dataset.pred2bbox(page.shape, true_boxes):
+            if confidence > 0:
+                cv2.rectangle(page, (left, top),
+                              (right, bottom), (0, 255, 0), 2)
     if wait:
         cv2.imshow("predict", page)
         return cv2.waitKey(0) != ord('q')
@@ -186,26 +212,20 @@ def draw(page: MatLike, boxes: list[PredictedBox], wait: bool = True) -> bool:
         return True
 
 
-def do_predict(model: nn.Module, page: MatLike):
-    # Loads and prepares the image.
-    h, w, _ = page.shape
-    scale = Dataset.max_width / w
-    page = cv2.resize(
-        page, (Dataset.max_width, int(h * scale)), interpolation=cv2.INTER_AREA
-    )
-
-    input = Dataset.transform(page)
+def do_predict(model: nn.Module,
+               page: MatLike,
+               true_boxes: Optional[torch.Tensor] = None) -> bool:
+    input, _ = Dataset.transform(page)
 
     yhat = model.forward(input.unsqueeze(0).unsqueeze(0))[0]
-    boxes = Dataset.pred2bbox(yhat)
-    draw(page, boxes)
+    return draw(page, yhat, true_boxes)
 
 
 class Log:
 
     path: Path
 
-    mse_losses: list[float]
+    iou_losses: list[float]
     bce_losses: list[float]
 
     def __init__(self, path=LOG_PATH):
@@ -216,21 +236,21 @@ class Log:
         if self.path.exists():
             with open(self.path, "r") as fp:
                 obj = json.load(fp)
-            self.mse_losses = obj["mse_losses"]
+            self.iou_losses = obj["iou_losses"]
             self.bce_losses = obj["bce_losses"]
         else:
             self.bce_losses = list()
-            self.mse_losses = list()
+            self.iou_losses = list()
 
     def save(self):
         with open(self.path, "w+") as fp:
             json.dump({
-                "mse_losses": self.mse_losses,
+                "iou_losses": self.iou_losses,
                 "bce_losses": self.bce_losses
             }, fp, indent=4)
 
-    def log(self, mse_loss: float, bce_loss: float):
-        self.mse_losses.append(mse_loss)
+    def log(self, iou_loss: float, bce_loss: float):
+        self.iou_losses.append(iou_loss)
         self.bce_losses.append(bce_loss)
         self.save()
 
@@ -245,7 +265,7 @@ def train(path: Path, epochs: int):
     logging.info(f"Using device {device}")
     model = StafferModel().to(device)
 
-    mse = nn.MSELoss()
+    iou = torchvision.ops.distance_box_iou_loss
     bce = nn.BCEWithLogitsLoss()
 
     log = Log()
@@ -258,8 +278,8 @@ def train(path: Path, epochs: int):
     last_report_count = 0
 
     # Mask to extract boxes 5-tuples
-    bbox_mask = torch.full((Dataset.batch_size, 40), True, dtype=torch.bool)
-    bbox_mask[:, 4::5] = False
+    bbox_mask = torch.full((Dataset.batch_size, 8, 5), True, dtype=torch.bool)
+    bbox_mask[:, :, 4::5] = False
 
     start_time = time.time()
     for epoch in range(0, epochs):
@@ -271,12 +291,19 @@ def train(path: Path, epochs: int):
             pages, y = pages.to(device), y.to(device)
             yhat = model.forward(pages.unsqueeze(1))
 
-            y_prob, yhat_prob = y[:, 4::5], yhat[:, 4::5]
+            # Extracts the confidence from the batch and the model output.
+            y_prob, yhat_prob = y[:, :, 4::5], yhat[:, :, 4::5]
             y_bboxes, yhat_bboxes = y[bbox_mask], yhat[bbox_mask]
 
-            mse_loss = mse(y_bboxes, yhat_bboxes)
+            mask = (y_bboxes != ds.no_box)
+            y_bboxes, yhat_bboxes = y_bboxes[mask], yhat_bboxes[mask]
+
+            iou_loss = iou(
+                yhat_bboxes.view((-1, 4)),
+                y_bboxes.view((-1, 4)),
+                reduction="mean")
             bce_loss = bce(yhat_prob, y_prob)
-            loss = 3 * mse_loss + bce_loss
+            loss = iou_loss + bce_loss
             loss.backward()
             opt.step()
 
@@ -291,11 +318,13 @@ def train(path: Path, epochs: int):
                     f"batch {batchno}/{batch_per_epochs}, " +
                     f"{count} samples " +
                     f"in {(now - start_time):.2f}s " +
+                    f"iou_loss: {iou_loss.item():2.5f}, "
+                    f"bce_loss: {bce_loss.item():2.5f}, "
                     f"loss: {loss.item():2.5f}"
                 )
 
                 # Tracks the losses.
-                log.log(mse_loss.item(), bce_loss.item())
+                log.log(iou_loss.item(), bce_loss.item())
                 start_time = time.time()
 
         torch.save({
@@ -317,14 +346,17 @@ def predict(model_path: Path, image_path: Optional[Path] = None):
     obj = torch.load(model_path, weights_only=True)
     model = StafferModel()
     model.load_state_dict(obj["state_dict"])
+    model.eval()
 
     if image_path is not None:
         do_predict(model, cv2.imread(str(image_path)))
     else:
         ds = Dataset()
         while True:
-            page = ds.pick_one()
-            do_predict(model, page)
+            page, boxes = ds.pick_one()
+            # draw(page.numpy(), boxes)
+            if not do_predict(model, page.numpy(), true_boxes=boxes):
+                break
 
 
 @click.command()
@@ -332,8 +364,7 @@ def draw_batch():
     ds = Dataset()
     pages, boxes = ds.batch()
     for i in range(0, len(pages)):
-        page_boxes = ds.pred2bbox(boxes[i], is_logits=False)
-        if not draw(pages[i].numpy(), page_boxes, wait=True):
+        if not draw(pages[i].numpy(), boxes[i], wait=True):
             break
 
 
@@ -358,22 +389,22 @@ def plot(log_path: Path = LOG_PATH, smooth: bool = True):
 
     fig, ax = plt.subplots()
     fig.canvas.mpl_connect('key_press_event', on_key)
-    mse_plot, = ax.plot([], [], 'b', label='MSE loss.')
+    iou_plot, = ax.plot([], [], 'b', label='IOU loss.')
     bce_plot, = ax.plot([], [], 'r', label='BCE loss.')
     print("Press 'q' to quit.")
 
     while not quit:
         log.load()
         if smooth:
-            mse_losses = moving_average(
-                np.array(log.mse_losses, dtype=np.float32))
+            iou_losses = moving_average(
+                np.array(log.iou_losses, dtype=np.float32))
             bce_losses = moving_average(
                 np.array(log.bce_losses, dtype=np.float32))
         else:
-            mse_losses = log.mse_losses
+            iou_losses = log.iou_losses
             bce_losses = log.bce_losses
-        mse_plot.set_xdata(range(0, len(mse_losses)))
-        mse_plot.set_ydata(mse_losses)
+        iou_plot.set_xdata(range(0, len(iou_losses)))
+        iou_plot.set_ydata(iou_losses)
         bce_plot.set_xdata(range(0, len(bce_losses)))
         bce_plot.set_ydata(bce_losses)
         ax.relim()
@@ -381,6 +412,12 @@ def plot(log_path: Path = LOG_PATH, smooth: bool = True):
         ax.legend()
         fig.canvas.draw_idle()
         plt.pause(1)
+
+
+@click.command()
+def stats():
+    ds = Dataset()
+    ds.stats()
 
 
 @click.group()
@@ -392,6 +429,7 @@ cli.add_command(train)
 cli.add_command(predict)
 cli.add_command(draw_batch)
 cli.add_command(plot)
+cli.add_command(stats)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
